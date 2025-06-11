f99331e1aedadef8679f891db4774c53f78ceae3
Navelogic <weslleysantoszbd@gmail.com>
2025-06-11 01:10:12 -0300

    Adicionando enums para tipos de transmissão e modelos de distribuição

---
+++ Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/Util/Enum/BroadcastTypeEnum.java
@@ -0,0 +1,6 @@
+package com.github.navelogic.estudiovirtualapi.Util.Enum;
+
+public enum BroadcastTypeEnum {
+    TRADITIONAL_TV,
+    STREAMING_PLATFORM
+}
---
+++ Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/Util/Enum/DistributionModelEnum.java
@@ -0,0 +1,7 @@
+package com.github.navelogic.estudiovirtualapi.Util.Enum;
+
+public enum DistributionModelEnum {
+    THEATRICAL_RELEASE,
+    STREAMING_EXCLUSIVE,
+    HYBRID_RELEASE
+}
