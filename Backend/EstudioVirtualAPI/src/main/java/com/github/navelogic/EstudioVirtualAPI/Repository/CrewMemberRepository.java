146eee58718f1517b336d7571992cf79f524f078
Navelogic <weslleysantoszbd@gmail.com>
2025-06-12 22:37:49 -0300

    Add import for List in CrewMemberRepository

--- Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/Repository/CrewMemberRepository.java
+++ Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/Repository/CrewMemberRepository.java
@@ -4,6 +4,8 @@ import com.github.navelogic.estudiovirtualapi.Model.CrewMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CrewMemberRepository extends JpaRepository<CrewMember, Long> {
 }
