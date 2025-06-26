5a43faec41a549e18618f0d6e02d2dd5846788e6
Navelogic <weslleysantoszbd@gmail.com>
2025-06-26 00:35:17 -0300

    Cria base para auditoria futura.

--- Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/EstudioVirtualApiApplication.java
+++ Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/EstudioVirtualApiApplication.java
@@ -2,10 +2,12 @@ package com.github.navelogic.estudiovirtualapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class EstudioVirtualApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EstudioVirtualApiApplication.class, args);
