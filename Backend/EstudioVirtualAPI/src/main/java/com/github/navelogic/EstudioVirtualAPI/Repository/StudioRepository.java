00a5093fa56312c46774daa540e6d917a1e38ced
Navelogic <weslleysantoszbd@gmail.com>
2025-07-16 14:39:24 -0300

    feat: search method to find a studio who is AI

--- Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/Repository/StudioRepository.java
+++ Backend/EstudioVirtualAPI/src/main/java/com/github/navelogic/EstudioVirtualAPI/Repository/StudioRepository.java
@@ -4,6 +4,9 @@ import com.github.navelogic.estudiovirtualapi.Model.Studio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudioRepository extends JpaRepository<Studio, Long> {
+    List<Studio> findByIsAiControlled(boolean isAiControlled);
 }
